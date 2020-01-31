f97485cea8ceacce6bf79c07eb89ddc855e190dc
Vit Stanislav <slaweet@gmail.com>
2020-01-31 23:37:59 +0100

    ✅ Fix unit test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -2,11 +2,8 @@ import { shallowMount } from '@vue/test-utils';
 import CubeCrash from '@/components/CubeCrash.vue';
 
 describe('CubeCrash.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(CubeCrash, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+  it('renders with no message at first', () => {
+    const wrapper = shallowMount(CubeCrash, { });
+    expect(wrapper.text()).toMatch('');
   });
 });
